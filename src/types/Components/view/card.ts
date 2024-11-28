3cf3940c107459f1cc7b1aff7f458e264d04cf80
FranciscoJ <brunoloouuiis@gmail.com>
2024-11-28 20:48:02 +0300

    ready to go

--- src/types/Components/view/card.ts
+++ src/types/Components/view/card.ts
@@ -2,12 +2,7 @@ import { IProduct } from "../..";
 
 // Render card products
 export interface ICard {
-    render(data: IProduct): HTMLElement;
-}
-
-export interface ICardSettings {
     text: HTMLElement;
     button: HTMLElement;
     render(data: IProduct): HTMLElement;
 }
-
